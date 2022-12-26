431e5bcd5834c7ac15984c3ac181da826bc172ef
mottitag <mottitag@gmail.com>
2022-12-26 08:27:42 -0300

    Final modulo 2

--- dist/assets/js/codigo.js
+++ dist/assets/js/codigo.js
@@ -1,3 +1,20 @@
+"use strict";
+
+function cambiarFoto (){
+    const foto_perfil = document.getElementById("input-foto-perfil");
+    foto_perfil.setAttribute("class", "");
+    foto_perfil.addEventListener("change", e=>leerArchivo(e.currentTarget.files))
+    const leerArchivo = foto => {
+        const reader = new FileReader();
+        reader.readAsDataURL(foto[0]);
+        reader.addEventListener("load", e=> {
+        document.getElementById("foto-perfil").src = e.currentTarget.result;
+    })
+    foto_perfil.setAttribute("class","d-none");
+}
+
+}
+
 function editarHomeParrafo () {
     const p = document.getElementById("home_parrafo");
     p.setAttribute("contentEditable", "true");
@@ -8,6 +25,13 @@ function editarHomeH1 (){
     const h1 = document.getElementById("home_h1");
     h1.setAttribute("contentEditable", "true");
 }
+
+
+// ------------------ CAMBIAR FOTO DE PERFIL ---------------------
+// Selecciona la imagen para cambiar la foto de perfil.
+const btn_edit_foto = document.getElementById("btn-edit-foto");
+btn_edit_foto.onclick = cambiarFoto;
+
 
 // Hace clic en el boton editar parrafo del home
 const btn_edit_home_p = document.getElementById("btn-edit-home-p");
